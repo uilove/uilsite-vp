54dd56ccb17c84f6a1ccd990d289bd1df0affc23
weakow <w@weakow.com>
2019-05-25 00:25:18 +0800

    Add icons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  title: "UI♥",
+  lang: 'zh-CN',
+  title: "UI Love",
   description:
     "UI♥ (UI Love) 旨在传播现代的互联网产品设计理念与方法。互联网产品的生态日新月异，新技术层出不穷，要求设计师拥抱变化，快速更新自己的知识体系。",
+  head: [
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#d5001a' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/img/icon.png` }]
+  ],
   themeConfig: {
     logo: '/img/logo.svg',
     nav: [
